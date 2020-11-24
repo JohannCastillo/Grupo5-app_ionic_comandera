885dc8cd1cca2ac6b25027d0f22698afb8cadf1e
413831 <alepleins3pp@gmail.com>
2020-11-24 18:46:26 -0300

    Se crean 5 widgets esqueletos

---
+++ src/app/components/widgets/plato-mas-pedido/plato-mas-pedido.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-plato-mas-pedido',
+  templateUrl: './plato-mas-pedido.component.html',
+  styleUrls: ['./plato-mas-pedido.component.scss'],
+})
+export class PlatoMasPedidoComponent implements OnInit
+{
+  public highchart;
+  public data;
+  public chart;
+  public updateFromInput = false;
+  public Highcharts = Highcharts;
+  public chartConstructor = 'chart';
+  public chartOptions;
+  public chartCallback;
+
+  constructor() { }
+
+  ngOnInit() 
+  {
+    this.crearGrafico();
+  }
+
+  procesarDatos()
+  {
+
+    // this.meses.forEach(foto => 
+    // {
+    //   if (new Date(foto.fecha).getDay() == dia) 
+    //   {
+    //     console.log(foto);
+
+    //     if (foto.votos.length > mayorVotos) 
+    //     {
+    //       mayorVotos = foto.votos.length;
+    //       imgSrc = foto.url;
+    //       auxiliar = {
+    //         name: new Date(foto.fecha).toISOString(),
+    //         y: foto.votos.length
+    //       };
+    //     }
+    //   }
+    // });
+    // if(auxiliar)
+    // {
+    //   this.assets.push(imgSrc);
+    //   votosPorDia.push(auxiliar);
+    // }
+
+
+    // this.data = votosPorDia;
+    // console.log(this.data);
+  }
+
+  crearGrafico()
+  {
+
+    this.chartCallback = (chart) =>
+    {
+      setTimeout(() =>
+      {
+        chart.reflow();
+        this.chartOptions = {
+          chart: {
+            type: 'column',
+          },
+          title: {
+            text: 'Fruit Consumption'
+          },
+          xAxis: {
+            categories: [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12]
+          },
+          yAxis: {
+            title: {
+              text: 'Fruit eaten'
+            }
+          },
+          series: [{
+            name: 'Jane',
+            data: [1, 0, 4, 1, 2, 3, 6, 7, 8, 4, 1, 6]
+          }, {
+            name: 'John',
+            data: [1, 0, 4, 1, 2, 3, 6, 7, 8, 4, 1, 6]
+          }]
+        };
+      }, 0);
+    };
+    this.chartOptions = {};
+  }
+
+}
